3f46f2b9e6863976a6e7aa8c812960f887df5127
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-01-07 09:13:08 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -2,79 +2,139 @@ import { join, dirname } from 'path';
 import { createRequire } from 'module';
 import { fileURLToPath } from 'url';
 import { setupMaster, fork } from 'cluster';
-import { watchFile, unwatchFile } from 'fs';
 import cfonts from 'cfonts';
-import { createInterface } from 'readline';
+import readline from 'readline';
 import yargs from 'yargs';
-import chalk from 'chalk';
-console.log('\n✰ Iniciando Sistema...');
+import chalk from 'chalk'; 
+import fs from 'fs'; 
+import './config.js';
+
+const { PHONENUMBER_MCC } = await import('baileys');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
-const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
-const rl = createInterface(process.stdin, process.stdout);
-say('Kanekzn\nBot', {
-font: 'block',
-align: 'center',
-colors: ['white']
-});
-say(`Multi Device`, {
-font: 'chrome',
-align: 'center',
-colors: ['red']
-});
-say(`Developed by Cristian`, {
-font: 'console',
-align: 'center',
-colors: ['magenta']
-});
-var isRunning = false;
-function start(file) {
-if (isRunning) return;
-isRunning = true;
-let args = [join(__dirname, file), ...process.argv.slice(2)];
-say([process.argv[0], ...args].join(' '), {
-font: 'console',
-align: 'center',
-colors: ['green']
-});
-setupMaster({
-exec: args[0],
-args: args.slice(1),
-});
-let p = fork();
-p.on('message', data => {
-switch (data) {
-case 'reset':
-p.process.kill();
-isRunning = false;
-start.apply(this, arguments);
-break;
-case 'uptime':
-p.send(process.uptime());
-break;
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+let isRunning = false;
+
+const question = (texto) => new Promise((resolver) => rl.question(texto, resolver));
+
+console.log(chalk.yellow.bold('—◉ㅤIniciando sistema...'));
+
+function verificarOCrearCarpetaAuth() {
+  const authPath = join(__dirname, global.authFile);
+  if (!fs.existsSync(authPath)) {
+    fs.mkdirSync(authPath, { recursive: true });
+  }
 }
-});
-p.on('exit', (_, code) => {
-isRunning = false;
-console.error('🔴 Error:\n', code);
-process.exit();
-if (code === 0) return;
-watchFile(args[0], () => {
-unwatchFile(args[0]);
-start(file);
-});
-});
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-if (!opts['test'])
-if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim());
-});
+
+function verificarCredsJson() {
+  const credsPath = join(__dirname, global.authFile, 'creds.json');
+  return fs.existsSync(credsPath);
 }
-process.on('warning', (warning) => {
-if (warning.name === 'MaxListenersExceededWarning') {
-console.warn('🔴 Se excedió el límite de Listeners en:');
-console.warn(warning.stack);
+
+function formatearNumeroTelefono(numero) {
+  let formattedNumber = numero.replace(/[^\d+]/g, '');
+  if (formattedNumber.startsWith('+52') && !formattedNumber.startsWith('+521')) {
+    formattedNumber = formattedNumber.replace('+52', '+521');
+  } else if (formattedNumber.startsWith('52') && !formattedNumber.startsWith('521')) {
+    formattedNumber = `+521${formattedNumber.slice(2)}`;
+  } else if (formattedNumber.startsWith('52') && formattedNumber.length >= 12) {
+    formattedNumber = `+${formattedNumber}`;
+  } else if (!formattedNumber.startsWith('+')) {
+    formattedNumber = `+${formattedNumber}`;
+  }
+  return formattedNumber;
 }
-});
+
+function esNumeroValido(numeroTelefono) {
+  const numeroSinSigno = numeroTelefono.replace('+', '');
+  return Object.keys(PHONENUMBER_MCC).some(codigo => numeroSinSigno.startsWith(codigo));
+}
+
+async function start(file) {
+  if (isRunning) return;
+  isRunning = true;
+
+  say('The Mystic\nBot', {
+    font: 'chrome',
+    align: 'center',
+    gradient: ['red', 'magenta'],
+  });
+
+  say(`Bot creado por Bruno Sobrino`, {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta'],
+  });
+
+  verificarOCrearCarpetaAuth();
+
+  if (verificarCredsJson()) {
+    const args = [join(__dirname, file), ...process.argv.slice(2)];
+    setupMaster({ exec: args[0], args: args.slice(1) });
+    const p = fork();
+    return;
+  }
+
+  const opcion = await question(chalk.yellowBright.bold('—◉ㅤSeleccione una opción (solo el numero):\n') + chalk.white.bold('1. Con código QR\n2. Con código de texto de 8 dígitos\n—> '));
+
+  let numeroTelefono = '';
+  if (opcion === '2') {
+    const phoneNumber = await question(chalk.yellowBright.bold('\n—◉ㅤEscriba su número de WhatsApp:\n') + chalk.white.bold('◉ㅤEjemplo: +5219992095479\n—> '));
+    numeroTelefono = formatearNumeroTelefono(phoneNumber);
+    if (!esNumeroValido(numeroTelefono)) {
+      console.log(chalk.bgRed(chalk.white.bold('[ ERROR ] Número inválido. Asegúrese de haber escrito su numero en formato internacional y haber comenzado con el código de país.\n—◉ㅤEjemplo:\n◉ +5219992095479\n')));
+      process.exit(0);
+    }
+    process.argv.push(numeroTelefono);
+  }
+
+  if (opcion === '1') {
+    process.argv.push('qr');
+  } else if (opcion === '2') {
+    process.argv.push('code');
+  }
+
+  const args = [join(__dirname, file), ...process.argv.slice(2)];
+  setupMaster({ exec: args[0], args: args.slice(1) });
+
+  const p = fork();
+
+  p.on('message', (data) => {
+    console.log(chalk.green.bold('—◉ㅤRECIBIDO:'), data);
+    switch (data) {
+      case 'reset':
+        p.process.kill();
+        isRunning = false;
+        start.apply(this, arguments);
+        break;
+      case 'uptime':
+        p.send(process.uptime());
+        break;
+    }
+  });
+
+  p.on('exit', (_, code) => {
+    isRunning = false;
+    console.error(chalk.red.bold('[ ERROR ] Ocurrió un error inesperado:'), code);
+    p.process.kill();
+    isRunning = false;
+    start.apply(this, arguments);
+    if (process.env.pm_id) {
+      process.exit(1);
+    } else {
+      process.exit();
+    }
+  });
+
+  const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
+  if (!opts['test']) {
+    if (!rl.listenerCount()) {
+      rl.on('line', (line) => {
+        p.emit('message', line.trim());
+      });
+    }
+  }
+}
+
 start('main.js');
